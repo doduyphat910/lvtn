94ea06c984d860b291a7983dc3d93eb0ee5629f8
TranHoHiep <hiep.tranho.19@gmail.com>
2018-05-13 18:47:04 +0700

    add vn locale

---
+++ resources/lang/vn/admin.php
@@ -0,0 +1,112 @@
+<?php
+
+return [
+    'online'                => 'Đang hoạt động',
+    'login'                 => 'Đăng nhập',
+    'logout'                => 'Đăng xuất',
+    'setting'               => 'Cài đặt',
+    'name'                  => 'Tên',
+    'username'              => 'Tên đăng nhập',
+    'password'              => 'Mật khẩu',
+    'password_confirmation' => 'Nhập lại mật khẩu',
+    'remember_me'           => 'Nhớ tôi',
+    'user_setting'          => 'Cài đặt người dùng',
+    'avatar'                => 'Ảnh đại diện',
+
+    'list'         => 'Danh sách',
+    'new'          => 'Tạo mới',
+    'create'       => 'Tạo',
+    'delete'       => 'Xóa',
+    'remove'       => 'Di chuyển',
+    'edit'         => 'Chỉnh sửa',
+    'view'         => 'Xem',
+    'browse'       => 'Trình duyệt',
+    'reset'        => 'Tạo lại',
+    'export'       => 'Xuất',
+    'batch_delete' => 'Xóa hàng loạt',
+    'save'         => 'Lưu',
+    'refresh'      => 'Làm mới',
+    'order'        => 'Order',
+    'expand'       => 'Mở rộng',
+    'collapse'     => 'Thu lại',
+    'filter'       => 'Bộ lọc',
+    'close'        => 'Đóng',
+    'show'         => 'Xem',
+    'entries'      => 'Mục',
+    'captcha'      => 'Captcha',
+
+    'action'            => 'Hành động',
+    'title'             => 'Tiêu đề',
+    'description'       => 'Mô tả',
+    'back'              => 'Trở lại',
+    'back_to_list'      => 'Trở lại danh sách',
+    'submit'            => 'Lưu lại',
+    'menu'              => 'Menu',
+    'input'             => 'Nhập',
+    'succeeded'         => 'Thành công',
+    'failed'            => 'Thất bại',
+    'delete_confirm'    => 'Bạn có chắc chắn muốn xóa mục này không ?',
+    'delete_succeeded'  => 'Xóa thành công !',
+    'delete_failed'     => 'Xóa thất bại !',
+    'update_succeeded'  => 'Cập nhật thành công !',
+    'save_succeeded'    => 'Lưu thành công !',
+    'refresh_succeeded' => 'Làm mới thành công !',
+    'login_successful'  => 'Đăng nhập thành công',
+
+    'choose'       => 'Chọn',
+    'choose_file'  => 'Chọn tập tin',
+    'choose_image' => 'Chọn hình ảnh',
+
+    'more' => 'Nhiều hơn',
+    'deny' => 'Bạn không có quyền truy cập !',
+
+    'administrator' => 'Quản trị viên',
+    'roles'         => 'Vai trò',
+    'permissions'   => 'Quyền',
+    'slug'          => 'Slug',
+
+    'created_at' => 'Thời gian tạo',
+    'updated_at' => 'Thời gian cập nhật',
+
+    'alert' => 'Thông báo',
+
+    'parent_id' => 'Parent',
+    'icon'      => 'Icon',
+    'uri'       => 'URI',
+
+    'operation_log'       => 'Nhật ký hoạt động',
+    'parent_select_error' => 'Lỗi chọn Parent',
+
+    'pagination' => [
+        'range' => 'Đang hiển thị :first đến :last của :total mục',
+    ],
+
+    'role'       => 'Vai trò',
+    'permission' => 'Quyền',
+    'route'      => 'Route',
+    'confirm'    => 'Xác nhận',
+    'cancel'     => 'Hủy bỏ',
+
+    'http' => [
+        'method' => 'HTTP method',
+        'path'   => 'HTTP path',
+    ],
+    'all_methods_if_empty' => 'All methods if empty',
+
+    'all'           => 'Tất cả',
+    'current_page'  => 'Trang hiện tại',
+    'selected_rows' => 'Các hàng đã chọn',
+
+    'upload'     => 'Tải lên',
+    'new_folder' => 'Thư mục mới',
+    'time'       => 'Thời gian',
+    'size'       => 'Kích thước',
+
+    'listbox' => [
+        'text_total'         => 'Hiển thị tất cả {0}',
+        'text_empty'         => 'Danh sách trống',
+        'filtered'           => '{0} / {1}',
+        'filter_clear'       => 'Hiển thị tất cả',
+        'filter_placeholder' => 'Bộ lọc',
+    ],
+];
