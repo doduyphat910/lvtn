c073e0e7931d851f58469002c599bebc296be90f
Phat <phat.do@kyanon.digital>
2018-06-03 13:39:57 +0700

    completed order semester (student)

--- app/Http/Controllers/SubjectRegisterController.php
+++ app/Http/Controllers/SubjectRegisterController.php
@@ -19,6 +19,7 @@ use Encore\Admin\Facades\Admin;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class SubjectRegisterController extends Controller
 {
@@ -35,27 +36,39 @@ class SubjectRegisterController extends Controller
     }
     protected function grid()
     {
-        return User::grid(SemesterSubjects::class, function (Grid $grid) {	
+        return User::grid(Subjects::class, function (Grid $grid) {
              $semester = Semester::where('status', 1)->pluck('id');
-         	$subjects_id = SemesterSubjects::whereIn('semester_id',$semester)->pluck('subjects_id');
-			// $grid->model()->whereIn('id', $subjects_id);
-			// $grid->column('Mã môn học')->display(function() use($subjects_id){
-			// 	return Subjects::whereIn('id', $subjects_id)->pluck('subject_code');
-			// });
-            // $grid->name('Tên môn học')->display(function ($name){
-            //     return  '<a href="/user/subjectregister/' . $this->id . '/details">'.$name.'</a>';
-            // });
-            // // $grid->credits('Số tín chỉ');
-            // // $grid->credits_fee('Số tín chỉ học phí');
-            $grid->column('Học kỳ - Năm')->display(function () {
+             $subjects_id = SemesterSubjects::whereIn('semester_id',$semester)->orderBy('semester_id', 'DESC')->pluck('subjects_id')->toArray();
+             $field = '';
+             foreach ($subjects_id as $id) {
+                $field .= $id . ',';
+             }
+             $field = substr( $field , 0, strlen($field)-1);
+             $grid->model()->whereIn('id', $subjects_id)->orderBy(DB::raw('FIELD(id, '. $field .')'));
+             $grid->subject_code('Mã môn học');
+//             $grid->id('ID')->sortable();
+             $grid->name('Tên môn học')->display(function ($name){
+                 return  '<a href="/user/subjectregister/' . $this->id . '/details">'.$name.'</a>';
+             });
+
+             $grid->credits('Số tín chỉ');
+             $grid->credits_fee('Số tín chỉ học phí');
+             $grid->column('Học kỳ - Năm')->display(function () {
                 $id = $this->id;
                 $subject = Subjects::find($id);
                 $arraySemester = $subject->semester()->pluck('id')->toArray();
                 $name = array_map( function ($arraySemester){
                     $nameSemester = Semester::find($arraySemester)->name;
-                    if($nameSemester == 0 ) {
-                    	 $nameSemester = 'Học kì hè';
-                    } 
+                    switch ($nameSemester) {
+                        case 0 :
+                            $nameSemester = 'Học kì hè';
+                            break;
+                        case 1:
+                            $nameSemester = 'Học kì 1';
+                            break;
+                        case 2:
+                            $nameSemester = 'Học kì 2';
+                    }
                     $year = Semester::find($arraySemester)->year()->get();
                     $nameYear = $year['0']->name;
                     return "<span class='label label-info'>{$nameSemester} - {$nameYear}</span>"  ;
