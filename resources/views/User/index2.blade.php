c46168c2710bf9f4ae1c55a77eb3be5f914472c0
TranHoHiep <hiep.tranho.19@gmail.com>
2018-06-12 17:25:02 +0700

    update notification (admin & users)....

--- resources/views/User/index2.blade.php
+++ resources/views/User/index2.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ Admin::title() }}</title>
+    <title> Đăng ký môn học  </title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
@@ -74,6 +74,7 @@
 <script src="{{ admin_asset ("/vendor/laravel-admin/sweetalert/dist/sweetalert.min.js") }}"></script>
 {!! Admin::js() !!}
 <script src="{{ admin_asset ("/vendor/laravel-admin/laravel-admin/laravel-admin.js") }}"></script>
-
+<script> $(".grid-refresh").hide();
+</script>
 </body>
 </html>
